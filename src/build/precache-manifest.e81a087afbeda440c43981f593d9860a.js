84a2b3f2c456c8e850ab0df4f93402e514d4fb61
Lans JPierre <lanscyjeanpierre@gmail.com>
2020-04-28 16:17:42 -0400

    First Commit

---
+++ src/build/precache-manifest.e81a087afbeda440c43981f593d9860a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42fdaf57320695434e852b45bbd07f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363ebeba73b8462b4bdd",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "03d6dfdb49e1f0428b3f",
+    "url": "/static/js/2.1fc4d83f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fc4d83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363ebeba73b8462b4bdd",
+    "url": "/static/js/main.30d85898.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
